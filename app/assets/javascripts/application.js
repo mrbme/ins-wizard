215e86e8e76e8890bfbf75f72cf5386bc11b571e
blakeepstein1 <blakeepstein1@gmail.com>
2018-07-25 10:52:27 -0400

    updated ip thing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,11 +14,3 @@
 //= require jquery_ujs
 // require bootstrap-sprockets
 
-
-        $.getJSON("http://ip-api.com/json/?callback=?", function(data) {
-            var table_body = "";
-            $.each(data, function(k, v) {
-                table_body += "<tr><td>" + k + "</td><td><b>" + v + "</b></td></tr>";
-            });
-            $("#GeoResults").html(table_body);
-        });
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,9 @@
 class HomeController < ApplicationController
-   def index
-     response = HTTParty.get('http://ip-api.com/json')
-     obj = JSON.parse(response.body)
-     @region = obj["region"]
+  def index
+    response = HTTParty.get('http://ip-api.com/json')
+    obj = JSON.parse(response.body)
+    
+    @region = obj["region"]
+    @city = obj["city"]
   end
 end
-
